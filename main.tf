d8bccf8f615ba25389853db7b67d472a7afec4b3
k-ozer <kozer6161@gmail.com>
2026-03-16 03:37:21 +0000

    Remove terraform provider files

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.34"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "CI_sg" {
+  name        = "cloud-init-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cloud-init-sg"
+  }
+}
+
+resource "aws_instance" "EC2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.CI_sg.id]
+  user_data              = file("cloud-init.yaml")
+
+  tags = {
+    Name = "cloud-init"
+  }
+}
